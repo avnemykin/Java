3c101d61885d4387b33deede60debc1982b6a40d
IDVladislav <55453633+IDVladislav@users.noreply.github.com>
2019-11-11 16:14:31 +0300

    Update Оброботка массива задание 1.java

--- Оброботка массива задание 1.java
+++ Оброботка массива задание 1.java
@@ -2,6 +2,8 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+        boolean es = false;
+        boolean es10 = false;
 	int[] arr ;
      arr = new int[20];
      for(int i = 0;i < 20;i++) {
@@ -10,19 +12,27 @@ public class Main {
      }
      System.out.print("Четные числа : ");
      for (int i = 0 ; i < 20 ;i ++)
-     
+
      {
-       if (arr[i] % 2 == 0)
-              System.out.print( arr[i] + " , ");
+       if (arr[i] % 2 == 0) {
+           es = true;
+           System.out.print(arr[i] + " , ");
+       }
+       if (es = false)
+           System.out.println("Четных нет");
      }
      System.out.print("\b" + "\b");
         System.out.println(".");
         System.out.print("Числа оканчивающиеся на 0  : ");
         for (int i = 0 ; i < 20 ;i ++)
         {
-            if (arr[i] % 10 == 0)
-                    System.out.print(arr[i] + " , ");
-         }
+            if (arr[i] % 10 == 0) {
+                 es10 = true;
+                System.out.print(arr[i] + " , ");
+              }
+        }
+        if (es = false)
+            System.out.println("Оканчивающихся на 0 нет!");
         System.out.print("\b" + "\b");
         System.out.println(".");
     }
